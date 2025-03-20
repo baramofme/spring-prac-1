4e70e14fac59253b66983e772268d28d388ea17d
JihoonYi <baramofme@gmail.com>
2025-03-20 16:01:35 +0900

    톰캣 3.0 어노테이션 기능으로 web.xml 설정을 WebServlet 과 WebFilter 로 변경

--- src/main/java/kr/co/fastcampus/web/SimpleFilter.java
+++ src/main/java/kr/co/fastcampus/web/SimpleFilter.java
@@ -3,11 +3,15 @@ package kr.co.fastcampus.web;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
-
+@WebFilter(
+        filterName = "simpleFilter",
+        urlPatterns = "simple"
+)
 @Slf4j
 public class SimpleFilter implements Filter {
     @Override
