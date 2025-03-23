a4d34f71161c3dec61e6fd4f143a0274982dfcf5
JihoonYi <baramofme@gmail.com>
2025-03-23 15:38:04 +0900

    InitializingBean 의 afterPropertiesSet 으로 빈 생성 시 커넥션 생성 콜백 미리 호출

--- src/main/java/kr/co/fastcampus/cli/Main.java
+++ src/main/java/kr/co/fastcampus/cli/Main.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.sql.Connection;
 import java.sql.SQLException;
 
 @Slf4j
@@ -16,8 +17,12 @@ public class Main {
         logger.info("안녕 세상아!");
 
         ApplicationContext context = new ClassPathXmlApplicationContext("dao.xml");
-        Dao dao2 = (Dao) context.getBean("dao");
-        Dao dao = context.getBean("dao", Dao.class);
-        dao.query();
+//        Dao dao2 = (Dao) context.getBean("dao");
+//        Dao dao = context.getBean("dao", Dao.class);
+//        dao.query();
+
+        ConnectionFactory factory = (ConnectionFactory) context.getBean(ConnectionFactory.class);
+        Connection connection = factory.getConnection();
+        log.info("" + (connection != null));
     }
 }
