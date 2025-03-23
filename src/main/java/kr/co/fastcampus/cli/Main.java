df62a79329102916178026b4e373de50cc1d98b5
JihoonYi <baramofme@gmail.com>
2025-03-23 17:06:45 +0900

    context 의 init 과 close 를 인터페이스나 bean 프로퍼티로 지정해서 사용

--- src/main/java/kr/co/fastcampus/cli/Main.java
+++ src/main/java/kr/co/fastcampus/cli/Main.java
@@ -4,6 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.Lifecycle;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.sql.Connection;
@@ -16,13 +18,21 @@ public class Main {
     public static void main(String[] args) throws SQLException {
         logger.info("안녕 세상아!");
 
-        ApplicationContext context = new ClassPathXmlApplicationContext("dao.xml");
-//        Dao dao2 = (Dao) context.getBean("dao");
-//        Dao dao = context.getBean("dao", Dao.class);
-//        dao.query();
+        // conetext.close 하려면 Closable 이 필요해서. 상위 부모 타입으로
+        ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("dao.xml");
+//        ConnectionFactory factory = (ConnectionFactory) context.getBean(ConnectionFactory.class);
+//        Connection connection = factory.getConnection();
+//        log.info("" + (connection != null));
 
-        ConnectionFactory factory = (ConnectionFactory) context.getBean(ConnectionFactory.class);
-        Connection connection = factory.getConnection();
-        log.info("" + (connection != null));
+        Lifecycle lifecycle = context.getBean(Lifecycle.class);
+        lifecycle.start();
+        log.info(">> 1: "+ lifecycle.isRunning());
+        // dao.xml 등록된 모든 beans 의 detroy 메소드 실행됨
+        context.close();
+        log.info(">> 2: "+ lifecycle.isRunning());
+
+        // Dao dao2 = (Dao) context.getBean("dao");
+        // Dao dao = context.getBean("dao", Dao.class);
+        // dao.query();
     }
 }
